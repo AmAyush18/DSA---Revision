7ec7f76c529ef67811067b83cc283f74ced1e283
Ayush Dwivedi <amayush0818@gmail.com>
2025-05-27 23:59:53 +0530

    Next Per

---
+++ arrays/014_NextPermutation.ts
@@ -0,0 +1,12 @@
+/* 
+Intution Steps:
+- Longer Prefix Match
+- Figure out the break point a[i], where: a[i] < a[i + 1]
+*/
+
+function nextPermutation (nums: number[]) : number[] {
+
+
+
+    return [];
+}
