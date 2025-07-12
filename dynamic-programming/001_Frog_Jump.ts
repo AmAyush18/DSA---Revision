e7bc0805c257ff799aa1cfec04c950ddc0481e77
Ayush Dwivedi <amayush0818@gmail.com>
2025-07-12 16:12:30 +0530

    DP: 001 - Frog Jump (Tabulation)

--- dynamic-programming/001_Frog_Jump.ts
+++ dynamic-programming/001_Frog_Jump.ts
@@ -7,6 +7,7 @@ class Solution {
         return this.findMinCost(n - 1, height, dp);
     }
 
+    // Memoization 
     private findMinCost(n: number, height: number[], dp: number[]): number {
         if (n === 0) return 0;
 
@@ -21,5 +22,25 @@ class Solution {
 
         dp[n] = Math.min(left, right);
         return dp[n];
+    }
+
+    // Tabulation
+    private findMinCostTab(height: number[]) : number {
+        let n: number = height.length
+        let dp: number[] = new Array(n).fill(-1)
+
+        dp[0] = 0
+        dp[1] = height[0]
+
+        for (let i: number = 1; i <= n-1; i++) {
+            let left = dp[i - 1] + Math.abs(height[i] - height[i-1])
+            let right = Number.MAX_SAFE_INTEGER
+            if (i > 1) {
+                right = dp[i - 2] + Math.abs(height[i] - height[i-2])
+            }
+            dp[i] = Math.min(left, right);
+        }
+
+        return dp[n-1];
     }
 }
